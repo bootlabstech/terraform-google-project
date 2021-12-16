537023074b63e44688dd0e982007912167a0e77d
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-16 10:58:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# terraform-google-project [![Latest Release](https://img.shields.io/github/release/bootlabstech/terraform-google-project.svg)](https://github.com/bootlabstech/terraform-google-project/releases/latest)
+
+[![Bootlabstech][logo]](https://www.bootlabs.in)
+
+[logo]: https://www.bootlabs.in/wp-content/uploads/2020/09/logo.png
---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_project" "my_project" {
+  name                = var.name
+  project_id          = "${var.project_id_prefix}-${random_string.random}"
+  org_id              = var.org_id
+  folder_id           = var.folder_id
+  billing_account     = var.billing_account
+  auto_create_network = var.auto_create_network
+}
+
+resource "random_string" "random" {
+  length           = 6
+  special          = false
+  lower            = false
+  uppder           = false
+}
