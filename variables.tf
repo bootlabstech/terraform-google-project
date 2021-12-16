bff3ef1a20fd6d70f996d83af30bb9ed8202ae64
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-16 11:17:22 +0530

    fixing bugs

--- variables.tf
+++ variables.tf
@@ -18,14 +18,17 @@ variable "billing_account" {
 variable "org_id" {
   type        = string
   description = "The numeric ID of the organization this project belongs to. Changing this forces a new project to be created. Only one of org_id or folder_id may be specified. If the org_id is specified then the project is created at the top level. Changing this forces the project to be migrated to the newly specified organization."
+  default     = ""
 }
 
 variable "folder_id" {
   type        = string
   description = "The numeric ID of the folder this project should be created under. Only one of org_id or folder_id may be specified. If the folder_id is specified, then the project is created under the specified folder. Changing this forces the project to be migrated to the newly specified folder."
+  default     = ""
 }
 
 variable "auto_create_network" {
   type        = bool
-  description = falses
+  description = "Create the 'default' network automatically. Default false. If set to false, the default network will be deleted. Note that, for quota purposes, you will still need to have 1 network slot available to create the project successfully, even if you set auto_create_network to false, since the network will exist momentarily."
+  default     = true
 }
