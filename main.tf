01357da27f43e4103cc05834f21e0239cc594835
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2021-12-16 14:45:49 +0530

    first tag

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "google_project" "my_project" {
   name                = var.name
   project_id          = "${var.project_id_prefix}-${random_string.random.result}"
   org_id              = var.org_id
-  folder_id           = var.folder_id
   billing_account     = var.billing_account
   auto_create_network = var.auto_create_network
 }
--- variables.tf
+++ variables.tf
@@ -14,19 +14,12 @@ variable "billing_account" {
   description = "The alphanumeric ID of the billing account this project belongs to. The user or service account performing this operation with Terraform must have at minimum Billing Account User privileges (roles/billing.user) on the billing account."
 }
 
-// optional variables
 variable "org_id" {
   type        = string
   description = "The numeric ID of the organization this project belongs to. Changing this forces a new project to be created. Only one of org_id or folder_id may be specified. If the org_id is specified then the project is created at the top level. Changing this forces the project to be migrated to the newly specified organization."
-  default     = null
 }
 
-variable "folder_id" {
-  type        = string
-  description = "The numeric ID of the folder this project should be created under. Only one of org_id or folder_id may be specified. If the folder_id is specified, then the project is created under the specified folder. Changing this forces the project to be migrated to the newly specified folder."
-  default     = null
-}
-
+// optional variables
 variable "auto_create_network" {
   type        = bool
   description = "Create the 'default' network automatically. Default false. If set to false, the default network will be deleted. Note that, for quota purposes, you will still need to have 1 network slot available to create the project successfully, even if you set auto_create_network to false, since the network will exist momentarily."
