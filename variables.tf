6a92c1c0876c7534d75edf1005624346bc9478e6
Silpashree1 <94901964+Silpashree1@users.noreply.github.com>
2022-02-22 21:16:53 +0530

    updated data types

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "billing_account" {
   type        = string
   description = <<-EOT
   {
-   "type": "json",
+   "type": "api",
    "purpose": "autocomplete",
    "data": "/api/v1/autocomplete/billingid",
    "description": "The alphanumeric ID of the billing account this project belongs to."
@@ -25,7 +25,7 @@ variable "org_id" {
   type        = string
   description = <<-EOT
   {
-   "type": "json",
+   "type": "api",
    "purpose": "autocomplete",
    "data": "/api/v1/autocomplete/organizationID",
    "description": "The numeric ID of the organization this project belongs to. Changing this forces a new project to be created."
